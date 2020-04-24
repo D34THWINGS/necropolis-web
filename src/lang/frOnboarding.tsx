37d7e85c239689dedd78340e10358e59d00bb43b
d34thwings <d34thwings@gmail.com>
2020-04-25 00:54:02 +0200

    🐛 Fix intro texts

--- src/lang/frOnboarding.tsx
+++ src/lang/frOnboarding.tsx
@@ -13,40 +13,59 @@ const introTexts = [
     Des heures passent, des jours peut-être même, lorsqu&apos;un son de porte se fait entendre.
     <br />
     <br />
-    &quot;
     <span css={textColor('PURPLE')}>
-      Je viens vous sortir de là, ma reine. La Faucheuse soit louée, vous êtes saine.
+      Vous êtes là, ma reine&nbsp;?
+      <br />
+      Je viens vous sauver&nbsp;!
+      <br />
+      Vous êtes donc saine,
+      <br />
+      La mort soit louée&nbsp;!
     </span>
-    &quot;
   </Fragment>,
   <Fragment>
-    Votre stelle se déplace et une lumière vient vous réchauffer.
+    Votre stèle se déplace tandis qu’une lumière vient vous réchauffer.
     <br />
     <br />
     Au-dessus de vous se tient une créature décharnée portant des habits luxueux quoique déchirés.
   </Fragment>,
   <Fragment>
-    &quot;
     <span css={textColor('PURPLE')}>
-      Ma reine, bon retour parmi les vivants, ou devrais-je dire bon retour en ce monde. Mais la prochaine fois, faites
-      votre enterrement, je vous en supplie, dans une moins grosse tombe. Des heures passent, des jours peut-être même,
-      lorsqu&apos;un son de porte se fait entendre.
+      Il est si pesant&nbsp;!
+      <br />
+      La prochaine fois, pitié,
+      <br />
+      Enterrez-vous dans...
+      <br />
+      Un tombeau plus léger.
     </span>
-    &quot;
+    <br />
     <br />
     Dit-il en suant abondamment.
   </Fragment>,
   <Fragment>
-    &quot;
     <span css={textColor('PURPLE')}>
-      Il semblerait que vous ne me reconnaissiez plus. Je suis le Valet royal, votre fidèle serviteur Qui n&apos;aspire
-      qu&apos;à vous servir, ô, Marenne Déchue. Pour vous, je reconstruirais la cité avec ardeur.
+      Devant vous respire
+      <br />
+      L&apos;humble Valet Moulu&nbsp;!
+      <br />
+      Pour vous servir
+      <br />
+      Ô, Marenne Déchue
+      <br />
+      <br />
+      Oui, je ferais tout.
+      <br />
+      Vous aider à bâtir,
+      <br />
+      Et donner des coups,
+      <br />
+      Jusqu’à m’en maudire&nbsp;!
     </span>
-    &quot;
   </Fragment>,
   <Fragment>
     Le Valet vous fait signe d&apos;emprunter une porte, puis vous fais la visite des lieux. Vous parcourez les boyaux
-    d&apos;un château tombé en désuétude.
+    d&apos;un château en ruine.
     <br />
     <br />
     Bienvenue chez vous.
